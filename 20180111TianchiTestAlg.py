f7329a20cf1853dae64459960caf6fd0a19d94c0
wangywUST <31923913+wangywUST@users.noreply.github.com>
2018-01-11 20:35:51 +0800

    Algorithm1.0

--- 20180111TianchiTestAlg.py
+++ 20180111TianchiTestAlg.py
@@ -29,6 +29,29 @@ ysize = 421
 
 chunksize = xsize * ysize
 df = pd.read_csv(file, chunksize = chunksize)
+df1 = pd.read_csv(file, chunksize = chunksize)
 
-pathList = givePath(df, np.asarray([xCity[0], yCity[0]]), np.asarray([xCity[1], yCity[1]]), 
-                    xsize, ysize)
+for i in range(1, 11):
+    if(i == 1):
+        pathList = givePath(df, np.asarray([xCity[0], yCity[0]]), 
+                            np.asarray([xCity[i], yCity[i]]), 
+                            xsize, ysize)
+    else:
+        pathList = np.concatenate((pathList, givePath(df, np.asarray([xCity[0], yCity[0]]), 
+                                                      np.asarray([xCity[i], yCity[i]]), 
+                            xsize, ysize)), axis = 0)  
+
+df1_block = df1.get_chunk(chunksize)
+windGra = df1_block["wind"].values.reshape(xsize,ysize)
+x = np.linspace(1, xsize, xsize)
+y = np.linspace(1, ysize, ysize)
+X,Y = np.meshgrid(y, x)
+CS = plt.contour(X, Y, windGra,levels = [15],
+                 colors=('k',),linestyles=('-',),linewidths=(1,))
+CSF = plt.contourf(X, Y, windGra, 8, alpha=.95, cmap=plt.cm.cool)
+plt.clabel(CS, fmt = '%2.1d', colors = 'k', fontsize=14) #contour line labels
+plt.colorbar(CSF, shrink=0.8, extend='both')
+plt.scatter(yCity[1:11], xCity[1:11], marker='x', s=50, c = 'gold', zorder=10)
+plt.scatter(pathList[:,1], pathList[:, 0], marker='x', s=5, c = 'gold', zorder=10)
+plt.scatter(yCity[0], xCity[0], marker='*', s=50, c = 'gold', zorder=10)
+plt.show()
--- Functions/givePath.py
+++ Functions/givePath.py
@@ -6,6 +6,7 @@ Created on Thu Jan 11 15:52:37 2018
 """
 from heuristicSquareAlg import *
 import numpy as np
+from fillPath import *
 
 def givePath(df, Departure, destination, xsize, ysize):
     chunksize = xsize * ysize
@@ -18,11 +19,14 @@ def givePath(df, Departure, destination, xsize, ysize):
         lastLoc = newLoc.copy()
         df_block = df.get_chunk(chunksize)
         windGra = df_block["wind"].values.reshape(xsize,ysize)
-        (newLoc, flag) = heuristicSquareAlg(windGra, lastLoc, destination, timePieceMin)
-        if(i > 0):
-            locList = np.concatenate((locList, np.asarray([newLoc])), axis = 0)
+        (newLoc, flag) = heuristicSquareAlg(windGra, lastLoc.copy(), destination, timePieceMin)
+        print(i)
+        print(lastLoc)
+        print(newLoc)
+        if(i == 0):
+            locList = np.concatenate((np.asarray([locList]), fillPath(lastLoc, newLoc)), axis = 0)
         else:
-            locList = np.row_stack((locList, newLoc))   
+            locList = np.concatenate((locList, fillPath(lastLoc, newLoc)), axis = 0)
         if(flag == True):
             break
     return(locList)
