454f104c4a4151306a5f2b8862bb6f1884575261
Linlong Wu <wllzore@gmail.com>
2018-01-16 22:58:56 +0800

    data

--- Data_convert.py
+++ Data_convert.py
@@ -9,8 +9,8 @@ def Data_convert(Data, thre_wind):
     for k in range(Data.shape[0]):
         for i in range(Data.shape[1]):
             for j in range(Data.shape[2]):
-                if Date[k, i, j] < thre_wind:
-                    Date[k, i, j] = 0 if Date[k, i, j] < thre_wind else 1
+                if Data[k, i, j] < thre_wind:
+                    Data[k, i, j] = 0 if Date[k, i, j] < thre_wind else 1
     for k in range(Data.shape[0]-1):
-        Date[k,:,:] = Date[k, :, :] or Date[k + 1, :, :]
-    return Date
+        Data[k,:,:] = Data[k, :, :] or Data[k + 1, :, :]
+    return Data
